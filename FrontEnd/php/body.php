18705a14d82be1d9f632da234fa8d5efabed72af
Gracie-Curiel <graciecuriel7@gmail.com>
2025-04-30 10:04:12 -0700

    added

---
+++ FrontEnd/php/body.php
@@ -0,0 +1,51 @@
+<div class="container mt-5">
+  <div class="row g-4">
+    <!-- Card 1 -->
+    <div class="col-md-3">
+      <div class="card h-100">
+        <img src="computer.jpg" class="card-img-top" alt="Computer Science">
+        <div class="card-body">
+          <h5 class="card-title">Computer Science</h5>
+          <p class="card-text">Look at what classes are live</p>
+          <a href="cmps.php" class="btn btn-primary">Go here</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card 2 -->
+    <div class="col-md-3">
+      <div class="card h-100">
+        <img src="math.jpg" class="card-img-top" alt="Math">
+        <div class="card-body">
+          <h5 class="card-title">Math</h5>
+          <p class="card-text">Look at what classes are live</p>
+          <a href="math.php" class="btn btn-primary">Go here</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card 3 -->
+    <div class="col-md-3">
+      <div class="card h-100">
+        <img src="art.jpg" class="card-img-top" alt="Art">
+        <div class="card-body">
+          <h5 class="card-title">Art</h5>
+          <p class="card-text">Look at what classes are live</p>
+          <a href="art.php" class="btn btn-primary">Go here</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card 4 -->
+    <div class="col-md-3">
+      <div class="card h-100">
+        <img src="history.jpg" class="card-img-top" alt="History">
+        <div class="card-body">
+          <h5 class="card-title">History</h5>
+          <p class="card-text">Look at what classes are live</p>
+          <a href="history.php" class="btn btn-primary">Go here</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
